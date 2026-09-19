c98ecae9cc7ff768734e2b5343298b0e7fe09d62
runningpoem30 <aryaanandpathak30@gmail.com>
2026-09-19 18:55:16 +0530

    tests for aurora

--- infra/cdk/bin/ondc-pulse.ts
+++ infra/cdk/bin/ondc-pulse.ts
@@ -2,9 +2,15 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { FoundationStack } from '../lib/stacks/FoundationStack';
+import { DatabaseStack } from '../lib/stacks/DatabaseStack';
 
 const app = new cdk.App();
 new FoundationStack(app, 'OndcPulseFoundationStack', {
+  // ...
+});
+
+new DatabaseStack(app, 'OndcPulseDatabaseStack', {
+
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
